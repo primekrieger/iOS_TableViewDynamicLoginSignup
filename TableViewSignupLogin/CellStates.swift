1b6139265720725321a8e595f2202efc4af3dbb8
primekrieger <primekrieger@gmail.com>
2016-08-08 18:06:01 +0530

    Add CellStates

---
+++ TableViewSignupLogin/CellStates.swift
@@ -0,0 +1,30 @@
+//
+//  CellStates.swift
+//  TableViewSignupLogin
+//
+//  Created by Diwakar Kamboj on 08/08/16.
+//  Copyright © 2016 Diwakar Kamboj. All rights reserved.
+//
+
+import Foundation
+
+enum CellType {
+    case EmailTextField
+    case PasswordTextField
+    case NameTextField
+    case LoginButton
+    case SignupButton
+}
+
+let loginState: [CellType] = [
+    .EmailTextField,
+    .PasswordTextField,
+    .LoginButton
+]
+
+let signupState: [CellType] = [
+    .EmailTextField,
+    .NameTextField,
+    .PasswordTextField,
+    .LoginButton
+]
