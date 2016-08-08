8c2ec991b3f5d4a7606ac3db4332b3f996ce91c5
primekrieger <primekrieger@gmail.com>
2016-08-08 17:09:42 +0530

    Add cells

---
+++ TableViewSignupLogin/LoginButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoginButtonTableViewCell.swift
+//  TableViewSignupLogin
+//
+//  Created by Diwakar Kamboj on 08/08/16.
+//  Copyright © 2016 Diwakar Kamboj. All rights reserved.
+//
+
+import UIKit
+
+class LoginButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
